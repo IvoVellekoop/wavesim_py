b515205834d4faeb0c2472f6171d3808cfa71a00
swapnilsmache <swapnilmache@gmail.com>
2024-06-11 17:40:50 +0200

    cluster testing multidomain 2 gpus

--- run_example.py
+++ run_example.py
@@ -6,10 +6,20 @@ from wavesim.helmholtzdomain import HelmholtzDomain
 from wavesim.multidomain import MultiDomain
 from anysim import run_algorithm  # to run the anysim iteration
 from utilities import preprocess
+import numpy as np
 
 
 # generate a refractive index map
-n_size = (1560, 360, 360)  # Size of the simulation domain
+sim_size = np.array([400, 200, 200])  # Simulation size in micrometers
+wavelength = 1.
+pixel_size = 0.25
+boundary_widths = 20
+
+# Size of the simulation domain
+n_size = sim_size * wavelength / pixel_size  # Size of the simulation domain in pixels
+n_size = n_size - 2 * boundary_widths  # Subtract the boundary widths
+n_size = n_size.astype(int)  # Convert to integer for indexing
+
 np.random.seed(0)
 n = np.random.normal(1.3, 0.1, n_size) + 1j * np.maximum(np.random.normal(0.05, 0.02, n_size), 0.0)
 
@@ -17,12 +27,9 @@ n = np.random.normal(1.3, 0.1, n_size) + 1j * np.maximum(np.random.normal(0.05, 
 source = np.zeros_like(n)  # Source term
 source[tuple(i // 2 for i in n_size)] = 1.  # Source term at the center of the domain
 
-boundary_widths = 20
 n, source = preprocess(n, source, boundary_widths)  # add boundary conditions and return permittivity and source
 
 # other parameters
-wavelength = 1.
-pixel_size = 0.25
 # periodic = (True, True, True)  # periodic boundary conditions, no wrapping correction.
 periodic = (False, True, True)  # wrapping corrections
 n_domains = (2, 1, 1)  # number of domains in each direction

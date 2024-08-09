c51aaf77b861fb3240c1921fad88a96ab50c81f1
s.mache@utwente.nl <maches@hpc-head1.local>
2024-08-09 10:13:47 +0200

    Improved memory management. Reduced memory overhead for MultiDomain, in theory, to < 0.005%

--- wavesim/iteration.py
+++ wavesim/iteration.py
@@ -21,7 +21,6 @@ def run_algorithm(domain: Domain, source, alpha=0.75, max_iterations=1000, thres
     # compute initial residual
     domain.add_source(slot_x, weight=1.)  # [x] = y
     preconditioner(domain, slot_x, slot_x)  # [x] = B(L+1)⁻¹y
-    empty_cache()  # free up memory before starting the simulation
     init_norm_inv = 1 / domain.inner_product(slot_x, slot_x)  # inverse of initial norm: 1 / norm([x])
     domain.clear(slot_x)  # Clear [x]
 

e1d6faa70117c8113ca71dbdb1a6769713f1045a
swapnilsmache <swapnilmache@gmail.com>
2024-08-14 12:03:54 +0200

    added low pass filtering of n to remove sharp edges in disordered medium

--- examples/run_example.py
+++ examples/run_example.py
@@ -3,11 +3,14 @@ import sys
 import torch
 import numpy as np
 from time import time
+# from torch.fft import fftn, ifftn, fftshift
+# import matplotlib.pyplot as plt
+# from matplotlib import colors
 sys.path.append(".")
 # from wavesim.helmholtzdomain import HelmholtzDomain
 from wavesim.multidomain import MultiDomain
 from wavesim.iteration import run_algorithm
-from wavesim.utilities import preprocess
+from wavesim.utilities import preprocess, normalize
 
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 os.environ["TORCH_USE_CUDA_DSA"] = "1"
@@ -15,11 +18,12 @@ if os.path.basename(os.getcwd()) == 'examples':
     os.chdir('..')
 
 # generate a refractive index map
-sim_size = 280 * np.array([1, 1, 1])  # Simulation size in micrometers
-wavelength = 1.
-pixel_size = 0.25
-boundary_widths = 20
-n_dims = len(sim_size.squeeze())
+sim_size = 50 * np.array([1, 1, 1])  # Simulation size in micrometers
+wavelength = 1.  # Wavelength in micrometers
+pixel_size = wavelength/4  # Pixel size in wavelength units
+boundary_wavelengths = 5  # Boundary width in wavelengths
+boundary_widths = int(boundary_wavelengths * wavelength / pixel_size)  # Boundary width in pixels
+n_dims = len(sim_size.squeeze())  # Number of dimensions
 
 # Size of the simulation domain
 n_size = sim_size * wavelength / pixel_size  # Size of the simulation domain in pixels
@@ -28,13 +32,23 @@ n_size = tuple(n_size.astype(int))  # Convert to integer for indexing
 
 torch.manual_seed(0)  # Set the random seed for reproducibility
 n = (torch.normal(mean=1.3, std=0.1, size=n_size, dtype=torch.float32)
-     + 1j * abs(torch.normal(mean=0.05, std=0.02, size=n_size, dtype=torch.float32))).numpy()
+     + 1j * abs(torch.normal(mean=0.05, std=0.02, size=n_size, dtype=torch.float32)))
+
+# # low pass filter to remove sharp edges
+# n_fft = fftn(n)
+# W = lambda x: torch.cat((torch.zeros(1, int(x*3/10)), 
+#                           torch.ones(1, int(x*4/10)), 
+#                           torch.zeros(1, int(x*3/10))), dim=1)
+# window = (W(n_size[1]).T @ W(n_size[0])).T[:,:,None] * (W(n_size[2])).view(1, 1, n_size[2])
+# n = ifftn(torch.multiply(n_fft, fftshift(window)))
+# n = torch.clamp(n.real, min=1.0) + 1.0j * torch.clamp(n.imag, min=0.0)
+
 print(f"Size of n: {n_size}")
 print(f"Size of n in GB: {n.nbytes / (1024**3):.2f}")
 assert n.imag.min() >= 0, 'Imaginary part of n is negative'
 
 # add boundary conditions and return permittivity (n²) and boundary_widths in format (ax0, ax1, ax2)
-n, boundary_array = preprocess(n, boundary_widths)
+n, boundary_array = preprocess(n.numpy(), boundary_widths)
 assert n.imag.min() >= 0, 'Imaginary part of n² is negative'
 
 # set up source, with size same as n + 2*boundary_widths, and a point source at the center of the domain
@@ -85,8 +99,8 @@ np.savez_compressed(f'{file_name}.npz', u=u)  # save the field
 
 # %% plot the field
 # extent = np.array([0, n_size[0], n_size[1], 0])*pixel_size
-# u = np.abs(u[:, :, u.shape[2]//2])
-# plt.imshow(u, cmap='hot_r', extent=extent)
+# u = normalize(np.abs(u[:, :, u.shape[2]//2]))
+# plt.imshow(u, cmap='hot_r', extent=extent, norm=colors.LogNorm())
 # plt.xlabel(r'$x~(\mu m)$')
 # plt.ylabel(r'$y~(\mu m)$')
 # cbar = plt.colorbar(fraction=0.046, pad=0.04)

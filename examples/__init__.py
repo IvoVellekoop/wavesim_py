ab846a801797255872b9d89a8b9cc671cb3ba06d
swapnilsmache <swapnilmache@gmail.com>
2024-11-28 12:18:03 +0100

    run_example.py updated

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,6 +1,85 @@
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal.windows import gaussian
+from scipy.fft import fftn, ifftn, fftshift
 from wavesim.utilities import normalize, relative_error
+
+
+def random_permittivity(shape):
+    np.random.seed(0)  # Set the random seed for reproducibility
+    n = (1.0 + np.random.rand(*shape).astype(np.float32) +
+         0.03j * np.random.rand(*shape).astype(np.float32))  # Random refractive index
+    n = smooth_n(n, shape)  # Low pass filter to remove sharp edges
+    n.real = normalize(n.real, a=1.0, b=2.0)  # Normalize to [1, 2]
+    n.imag = normalize(n.imag, a=0.0, b=0.03)  # Normalize to [0, 0.03]
+    # make sure that the imaginary part of n² is positive
+    mask = (n ** 2).imag < 0
+    n.imag[mask] *= -1.0
+
+    n[0:5, :, :] = 1
+    n[-5:, :, :] = 1
+    return n ** 2
+
+
+def smooth_n(n, shape):
+    """Low pass filter to remove sharp edges"""
+    n_fft = fftn(n)
+    w = (window(shape[1]).T @ window(shape[0])).T[:, :, None] * window(shape[2]).reshape(1, 1, shape[2])
+    n = ifftn(n_fft * fftshift(w))
+    n = np.clip(n.real, a_min=1.0, a_max=None) + 1.0j * np.clip(n.imag, a_min=0.0, a_max=None)
+
+    assert (n ** 2).imag.min() >= 0, 'Imaginary part of n² is negative'
+    assert n.shape == shape, 'n and shape do not match'
+    assert n.dtype == np.complex64, 'n is not complex64'
+    return n
+
+
+def window(x):
+    """Create a window function for low pass filtering"""
+    c0 = round(x / 4)
+    cl = (x - c0) // 2
+    cr = cl
+    if c0 + cl + cr != x:
+        c0 = x - cl - cr
+    return np.concatenate((np.zeros((1, cl), dtype=np.complex64), 
+                           np.ones((1, c0), dtype=np.complex64), 
+                           np.zeros((1, cr), dtype=np.complex64)), axis=1)
+
+
+def construct_source(source_type, at, shape):
+    if source_type == 'point':
+        return torch.sparse_coo_tensor(at[:, None], torch.tensor([1.0]), shape, dtype=torch.complex64)
+    elif source_type == 'plane_wave':
+        return source_plane_wave(at, shape)
+    elif source_type == 'gaussian_beam':
+        return source_gaussian_beam(at, shape)
+    else:
+        raise ValueError(f"Unknown source type: {source_type}")
+
+
+def source_plane_wave(at, shape):
+    """ Set up source, with size same as permittivity, 
+        and a plane wave source on one edge of the domain """
+    # TODO: use CSR format instead?
+    data = np.ones((1, shape[1], shape[2]), dtype=np.float32)  # the source itself
+    return torch.sparse_coo_tensor(at, data, shape, dtype=torch.complex64)
+
+
+def source_gaussian_beam(at, shape):
+    """ Set up source, with size same as permittivity, 
+        and a Gaussian beam source on one edge of the domain """
+    # TODO: use CSR format instead?
+    std = (shape[1] - 1) / (2 * 3)
+    source_amplitude = gaussian(shape[1], std).astype(np.float32)
+    source_amplitude = np.outer(gaussian(shape[2], std).astype(np.float32), 
+                                source_amplitude.astype(np.float32))
+    source_amplitude = torch.tensor(source_amplitude[None, ...])
+    data = torch.zeros((1, shape[1], shape[2]), dtype=torch.complex64)
+    data[0, 
+           0:shape[1], 
+           0:shape[2]] = source_amplitude
+    return torch.sparse_coo_tensor(at, data, shape, dtype=torch.complex64)
 
 
 def plot(x, x_ref, re=None, normalize_x=True):
--- examples/run_example.py
+++ examples/run_example.py
@@ -6,6 +6,7 @@ Example script to run a simulation of a point source in a random refractive inde
 
 import os
 import sys
+
 import torch
 import numpy as np
 from time import time
@@ -13,7 +14,9 @@ from scipy.signal.windows import gaussian
 from torch.fft import fftn, ifftn, fftshift
 import matplotlib.pyplot as plt
 from matplotlib import colors
+
 sys.path.append(".")
+from __init__ import random_permittivity, construct_source
 from wavesim.helmholtzdomain import HelmholtzDomain
 from wavesim.multidomain import MultiDomain
 from wavesim.iteration import run_algorithm
@@ -26,85 +29,36 @@ if os.path.basename(os.getcwd()) == 'examples':
 
 # generate a refractive index map
 sim_size = 50 * np.array([1, 1, 1])  # Simulation size in micrometers
+periodic = (False, True, True)
 wavelength = 1.  # Wavelength in micrometers
 pixel_size = wavelength/4  # Pixel size in wavelength units
 boundary_wavelengths = 10  # Boundary width in wavelengths
-boundary_widths = int(boundary_wavelengths * wavelength / pixel_size)  # Boundary width in pixels
+boundary_widths = [int(boundary_wavelengths * wavelength / pixel_size), 0, 0]  # Boundary width in pixels
 n_dims = len(sim_size.squeeze())  # Number of dimensions
 
 # Size of the simulation domain
 n_size = sim_size * wavelength / pixel_size  # Size of the simulation domain in pixels
-n_size = n_size - 2 * boundary_widths  # Subtract the boundary widths
 n_size = tuple(n_size.astype(int))  # Convert to integer for indexing
 
-torch.manual_seed(0)  # Set the random seed for reproducibility
-n = (torch.normal(mean=1.3, std=0.1, size=n_size, dtype=torch.float32)
-     + 1j * abs(torch.normal(mean=0.01, std=0.01, size=n_size, dtype=torch.float32)))
+# return permittivity (n²) with absorbing boundaries
+permittivity = random_permittivity(n_size)
+permittivity = preprocess(permittivity, boundary_widths)[0]
+assert permittivity.imag.min() >= 0, 'Imaginary part of n² is negative'
+assert (permittivity.shape == np.asarray(n_size) + 2*np.asarray(boundary_widths)).all(), 'permittivity and n_size do not match'
+assert permittivity.dtype == np.complex64, f'permittivity is not complex64, but {permittivity.dtype}'
 
-# low pass filter to remove sharp edges
-n_fft = fftn(n)
-def W(x):
-    c0 = int(x*7/20)
-    cl = (x - c0) // 2
-    cr = cl
-    if c0 + cl + cr != x:
-        c0 = x - cl - cr
-    return torch.cat((torch.zeros(1, cl), 
-                      torch.ones(1, c0), 
-                      torch.zeros(1, cr)), dim=1)
+# construct a source at the center of the domain
+source = construct_source(source_type='point', at=np.asarray(permittivity.shape) // 2, shape=permittivity.shape)
+# source = construct_source(source_type='plane_wave', at=[[boundary_widths[0]]], shape=permittivity.shape)
+# source = construct_source(source_type='gaussian_beam', at=[[boundary_widths[0]]], shape=permittivity.shape)
 
-window = (W(n_size[1]).T @ W(n_size[0])).T[:,:,None] * (W(n_size[2])).view(1, 1, n_size[2])
-n = ifftn(torch.multiply(n_fft, fftshift(window)))
-n = torch.clamp(n.real, min=1.0) + 1.0j * torch.clamp(n.imag, min=0.0)
-
-print(f"Size of n: {n_size}")
-print(f"Size of n in GB: {n.nbytes / (1024**3):.2f}")
-assert n.imag.min() >= 0, 'Imaginary part of n is negative'
-assert n.shape == n_size, 'n and n_size do not match'
-assert n.dtype == torch.complex64, 'n is not complex64'
-
-# return permittivity (n²) with boundaries, and boundary_widths in format (ax0, ax1, ax2)
-n, boundary_array = preprocess((n**2).numpy(), boundary_widths)  # permittivity is n², but uses the same variable n
-assert n.imag.min() >= 0, 'Imaginary part of n² is negative'
-assert (n.shape == np.asarray(n_size) + 2*boundary_array).all(), 'n and n_size do not match'
-assert n.dtype == np.complex64, f'n is not complex64, but {n.dtype}'
-
-# # set up source, with size same as n + 2*boundary_widths, and a point source at the center of the domain
-# indices = torch.tensor([[v//2 + boundary_array[i] for i, v in enumerate(n_size)]]).T  # Location: center of domain
-# values = torch.tensor([1.0])  # Amplitude: 1
-# n_ext = tuple(np.array(n_size) + 2*boundary_array)
-# source = torch.sparse_coo_tensor(indices, values, n_ext, dtype=torch.complex64)
-
-# set up source, with size same as n + 2*boundary_widths, and a plane wave source on one edge of the domain
-k_size = torch.tensor([n_size[0], n_size[2]], dtype=torch.float64)
-# choose |k| <  Nyquist, make sure k is at exact grid point in Fourier space
-k_relative = torch.tensor((0.2, -0.15), dtype=torch.float64)
-k = 2 * torch.pi * torch.round(k_relative * k_size) / k_size  # in 1/pixels
-source_amplitude = torch.exp(1j * (
-    k[0] * torch.arange(k_size[0]).reshape(-1, 1, 1) +
-    k[1] * torch.arange(k_size[1]).reshape(1, -1, 1))).squeeze_().to(torch.complex64)
-n_ext = tuple(np.array(n_size) + 2*boundary_array)
-source = torch.zeros(n_ext, dtype=torch.complex64)
-source[boundary_array[0]:boundary_array[0]+n_size[0], 
-       boundary_array[1], 
-       boundary_array[2]:boundary_array[2]+n_size[2]] = source_amplitude
-
-# convert source to sparse tensor
-indices = torch.nonzero(source, as_tuple=False)
-values = source[indices[:, 0], indices[:, 1], indices[:, 2]]
-source = torch.sparse_coo_tensor(indices.T, values, n_ext, dtype=torch.complex64)
-
-# # 1-domain, periodic boundaries (without wrapping correction)
-# periodic = (True, True, True)  # periodic boundaries, wrapped field.
+# # 1-domain
 # n_domains = (1, 1, 1)  # number of domains in each direction
-# domain = HelmholtzDomain(permittivity=n, periodic=periodic, wavelength=wavelength, pixel_size=pixel_size)
+# domain = HelmholtzDomain(permittivity=permittivity, periodic=periodic, wavelength=wavelength, pixel_size=pixel_size)
 
-# OR. Uncomment to test domain decomposition
-n_domains = np.array([1, 2, 1])  # number of domains in each direction
-periodic = np.where(n_domains == 1, True, False)  # True for 1 domain in that direction, False otherwise
-n_domains = tuple(n_domains)
-periodic = tuple(periodic)
-domain = MultiDomain(permittivity=n, periodic=periodic, wavelength=wavelength, pixel_size=pixel_size,
+# 1-domain or more with domain decomposition
+n_domains = (1, 1, 1)  # number of domains in each direction
+domain = MultiDomain(permittivity=permittivity, periodic=periodic, wavelength=wavelength, pixel_size=pixel_size,
                      n_domains=n_domains)
 
 start = time()
@@ -131,7 +85,8 @@ with open('logs/output.txt', 'a') as file:
 
 # %% crop and save the field
 # crop the field to the region of interest
-u = u.squeeze()[*([slice(boundary_widths, -boundary_widths)] * n_dims)].cpu().numpy()
+u = u.squeeze()[*([slice(boundary_widths[i], 
+                         u.shape[i] - boundary_widths[i]) for i in range(3)])].cpu().numpy()
 np.savez_compressed(f'{file_name}.npz', u=u)  # save the field
 
 # %% plot the field
